96eee2651cd14b33043ce68ae31d7a7b5107b5c1
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-10-11 13:08:08 +0200

    implement struct rendering

--- src/librustdoc/json/mod.rs
+++ src/librustdoc/json/mod.rs
@@ -11,6 +11,7 @@ use crate::error::Error;
 use crate::formats::{FormatRenderer, get_attributes};
 
 use rustc::util::nodemap::FxHashMap;
+use serialize::json::{ToJson, Json, as_json};
 use syntax::symbol::{Symbol, sym};
 use syntax::ast;
 use syntax::edition::Edition;
@@ -115,37 +116,77 @@ impl FormatRenderer for Context {
 impl Context {
     fn render_mod(&mut self, item: &clean::Item, nb_items: usize) -> Result<(), Error> {
         println!("render_mod");
-        self.write(&format!("{comma}{{\"name\":\"{name}\",\"doc\":\"{doc}\",\"items\":[",
-                            comma=if nb_items > 0 { "," } else { "" },
-                            name=item.name.as_ref().unwrap(),
-                            doc=item.doc_value()
-                                    .unwrap_or_else(|| "")
-                                    .replace("\\", "\\\\")
-                                    .replace("\"", "\\\""))) // replace with to json
+        self.write(
+            &format!("{comma}{{\"name\":{name},\"doc\":{doc},\"kind\":\"module\",\"items\":[",
+                     comma=if nb_items > 0 { "," } else { "" },
+                     name=item.name.as_ref().unwrap().to_json(),
+                     doc=item.doc_value().unwrap_or_else(|| "").to_json()))
     }
 
     fn render_item(&mut self, item: clean::Item, nb_items: usize) -> Result<(), Error> {
         println!("render_item");
-        let mut s = format!("{comma}{{\"name\":\"{name}\",\"doc\":\"{doc}\"",
-                            comma=if nb_items > 0 { "," } else { "" },
-                            name=item.name.as_ref().unwrap(),
-                            doc=item.doc_value()
-                                .unwrap_or_else(|| "")
-                                .replace("\\", "\\\\")
-                                .replace("\"", "\\\"")); // replace with to json
+        let mut buf = format!("{comma}{{\"name\":{name},\"doc\":{doc},\"kind\":{kind}",
+                              comma=if nb_items > 0 { "," } else { "" },
+                              name=item.name.as_ref().unwrap().to_json(),
+                              doc=item.doc_value().unwrap_or_else(|| "").to_json(),
+                              kind=item.type_().to_string().to_json());
         let attrs = get_attributes(&item);
         if !attrs.is_empty() {
-            s.push_str(&format!(",\"attributes\":[{}]",
-                                attrs
-                                    .iter()
-                                    .map(|a| format!("\"{}\"", a))
-                                    .collect::<Vec<_>>()
-                                    .join(","))); // replace with to json
+            buf.push_str(&format!(",\"attributes\":[{}]",
+                                  attrs
+                                      .iter()
+                                      .map(|a| a.to_json().to_string())
+                                      .collect::<Vec<_>>()
+                                      .join(","))); // replace with to json
         }
-        s.push_str("}");
-        self.write(&s)
+        match item.inner {
+            // clean::ModuleItem(ref m) =>
+            //     item_module(buf, cx, item, &m.items),
+            // clean::FunctionItem(ref f) | clean::ForeignFunctionItem(ref f) =>
+            //     item_function(buf, cx, item, f),
+            // clean::TraitItem(ref t) => item_trait(buf, cx, item, t),
+            clean::StructItem(ref s) => self.render_struct(&mut buf, &item, s),
+            // clean::UnionItem(ref s) => item_union(buf, cx, item, s),
+            // clean::EnumItem(ref e) => item_enum(buf, cx, item, e),
+            // clean::TypedefItem(ref t, _) => item_typedef(buf, cx, item, t),
+            // clean::MacroItem(ref m) => item_macro(buf, cx, item, m),
+            // clean::ProcMacroItem(ref m) => item_proc_macro(buf, cx, item, m),
+            // clean::PrimitiveItem(_) => item_primitive(buf, cx, item),
+            // clean::StaticItem(ref i) | clean::ForeignStaticItem(ref i) =>
+            //     item_static(buf, cx, item, i),
+            // clean::ConstantItem(ref c) => item_constant(buf, cx, item, c),
+            // clean::ForeignTypeItem => item_foreign_type(buf, cx, item),
+            // clean::KeywordItem(_) => item_keyword(buf, cx, item),
+            // clean::OpaqueTyItem(ref e, _) => item_opaque_ty(buf, cx, item, e),
+            // clean::TraitAliasItem(ref ta) => item_trait_alias(buf, cx, item, ta),
+            _ => {
+                // We don't generate pages for any other type.
+            }
+        }
+        buf.push_str("}");
+        self.write(&buf)
     }
 
+    fn render_struct(&self, w: &mut String, it: &clean::Item, s: &clean::Struct) {
+        /*s.generics
+        if let doctree::Plain = s.struct_type {
+            let mut fields = s.fields.iter().filter_map(|f| {
+            match f.inner {
+                clean::StructFieldItem(ref ty) => Some((f, ty)),
+                _ => None,
+            }
+        }).peekable();
+            write!(w, "<span id=\"{id}\" class=\"{item_type} small-section-header\">\
+                           <a href=\"#{id}\" class=\"anchor field\"></a>\
+                           <code id=\"{ns_id}\">{name}: {ty}</code>\
+                           </span>",
+                       item_type = ItemType::StructField,
+                       id = id,
+                       ns_id = ns_id,
+                       name = field.name.as_ref().unwrap(),
+                       ty = ty.print());
+                document(w, cx, field);*/
+    }
 
     fn write(&mut self, s: &str) -> Result<(), Error> {
         write!(*self.file.borrow_mut(), "{}", s).map_err(|e| Error::new(e, &*self.path))
