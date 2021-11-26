6496c75099efb9e420f270595b8792a280ee66b2
Yuval Dolev <yuvaldolev2@gmail.com>
2021-11-26 16:04:33 +0200

    Support Required trait functions and ForeignItem functions from fn_kind

--- compiler/rustc_resolve/src/late/lifetimes.rs
+++ compiler/rustc_resolve/src/late/lifetimes.rs
@@ -684,15 +684,16 @@ impl<'a, 'tcx> Visitor<'tcx> for LifetimeContext<'a, 'tcx> {
             intravisit::FnKind::ItemFn(id, _, _, _) => id.name,
             intravisit::FnKind::Method(id, _, _) => id.name,
             intravisit::FnKind::Closure => sym::closure,
+            intravisit::FnKind::ForeignFn(id) => id.name,
         };
         let name = name.as_str();
         let span = span!(Level::DEBUG, "visit_fn", name);
         let _enter = span.enter();
         match fk {
             // Any `Binders` are handled elsewhere
-            intravisit::FnKind::ItemFn(..) | intravisit::FnKind::Method(..) => {
-                intravisit::walk_fn(self, fk, fd, b, s, hir_id)
-            }
+            intravisit::FnKind::ItemFn(..)
+            | intravisit::FnKind::Method(..)
+            | intravisit::FnKind::ForeignFn(..) => intravisit::walk_fn(self, fk, fd, b, s, hir_id),
             intravisit::FnKind::Closure => {
                 self.map.late_bound_vars.insert(hir_id, vec![]);
                 let scope = Scope::Binder {
