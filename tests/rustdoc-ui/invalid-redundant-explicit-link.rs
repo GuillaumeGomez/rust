628175b39ec7b405237520cebfc18ed0d2c566fb
Guillaume Gomez <guillaume.gomez@huawei.com>
2024-03-28 11:09:08 +0100

    Add regression test for #123158

---
+++ tests/rustdoc-ui/invalid-redundant-explicit-link.rs
@@ -0,0 +1,9 @@
+//@ check-pass
+
+// Regression test for <https://github.com/rust-lang/rust/issues/123158>. It
+// should not emit any warning.
+
+//! [**`SomeTrait`**](SomeTrait):
+//! [*`SomeTrait`*](SomeTrait):
+
+pub trait SomeTrait {}
