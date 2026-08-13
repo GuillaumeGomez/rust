6730b6a004e8f6e434355ae0ce7cb03d78b0d33c
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-08-13 15:05:15 +0200

    Add extra checks to ensure GCC and binutils have needed support for the `retain` attribute

--- src/ci/docker/scripts/build-gcc.sh
+++ src/ci/docker/scripts/build-gcc.sh
@@ -11,12 +11,19 @@ hide_output ./configure
 hide_output make
 hide_output make install
 
+if echo '.section .test,"awR",@progbits' | as - -o /dev/null 2>/dev/null; then
+    echo "binutils assembler supports SHF_GNU_RETAIN"
+else
+    echo "binutils assembler DOES NOT support SHF_GNU_RETAIN"
+    exit 1
+fi
+
 # Note: in the future when bumping to version 10.1.0, also take care of the sed block below.
 # This version is specified in the Dockerfile
 GCC=$GCC_VERSION
 
 curl https://ci-mirrors.rust-lang.org/rustc/gcc/gcc-$GCC.tar.xz | xzcat | tar xf -
-cd gcc-$GCC
+cd ../gcc-$GCC
 
 # FIXME(#49246): Remove the `sed` below.
 #
@@ -54,6 +61,12 @@ hide_output ../gcc-$GCC/configure \
 hide_output make -j$(nproc)
 hide_output make install
 ln -s gcc /rustroot/bin/cc
+if echo 'int x __attribute__((used, retain));' | gcc -S -x c -o - - | grep -i '"a.*R"'; then
+    echo "retain attribute is supported"
+else
+    echo "retain attribute is not supported"
+    exit 1
+fi
 
 cd ..
 rm -rf gcc-build
